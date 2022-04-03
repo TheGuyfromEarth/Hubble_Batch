89705bd4b2996c25359779db78fec60c7de546d4
TheGuyfromEarth <91563278+TheGuyfromEarth@users.noreply.github.com>
2022-04-03 23:14:38 +0530

    Add files via upload
    update changes for linked list implementation

--- LinkedListImplementation/src/MyLinkedList.java
+++ LinkedListImplementation/src/MyLinkedList.java
@@ -4,50 +4,105 @@ public class MyLinkedList {
     Node head;
 
     // returns the number of nodes in the linked list
-    public int size(){
-        return 0;
+    public int size() {
+        Node temp = head;
+        int count = 0;
+
+        while (temp != null) {
+            count++;
+            temp = temp.next;
+        }
+        return count;
     }
 
 
     // returns the state of linked list
-    public boolean isEmpty(){
-        if(head == null)
+    public boolean isEmpty() {
+        if (head == null)
             return true;
 
         return false;
     }
 
     // adds a node to the end of the linked list
-    public void add(int data){
-
-        if (isEmpty()){
+    public void add(int data) {
+        // if list is empty, we create the very first node in the linked list
+        if (isEmpty()) {
             head = new Node(data);
         }
-        else{
-
+        // list is not empty
+        else {
             Node temp = head;
 
-            while (temp!=null){
+            while (temp.next != null) {
 
                 temp = temp.next;
             }
-            temp = new Node(data);
+            temp.next = new Node(data);
         }
 
     }
 
     // returns the value(data) in the node at the given index
-    public int get(int index){
-        return 0;
+    public int get(int index) {
+        // validating the index
+        if (index < 0 || index >= size())
+            throw new IllegalArgumentException("Invalid Index");
+
+        Node temp = head;
+        int count = 0;
+
+        while (count != index) {
+            temp = temp.next;
+            count++;
+        }
+        return temp.data;
     }
 
     // removes the corresponding node with the given data from the linked list
-    public void remove(int data){
+    public void remove(int data) {
+
+        if (isEmpty())
+            throw new RuntimeException("List is empty");
+
+        Node temp = head;
+        boolean isRemoved = false;
+
+        while (temp != null && !isRemoved) {
+            // at least second node is present
+            if (temp.next != null) {
+                // is the node to be removed first node itself
+                if (temp.data == data) {
+                    head = temp.next;
+                    temp.next = null;
+                    isRemoved = true;
+                } else {
+                    if (temp.next.data == data) {
+                        // bypass next node to make a connection from curr node to next's next node
+                        temp.next = temp.next.next;
+                        // break the link from next node to it's next mode
+                        temp.next.next = null;
+                        isRemoved = true;
+                    }
+                }
+            } else {
+                head = null;
+            }
+            temp = temp.next;
+        }
+
 
     }
 
     // prints the elements in the linked list
-    public void display(){
+    public void display() {
+
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
 
     }
 }
--- LinkedListImplementation/src/Test.java
+++ LinkedListImplementation/src/Test.java
@@ -4,8 +4,28 @@ public class Test {
 
         MyLinkedList myList = new MyLinkedList();
 
-        System.out.println(myList.isEmpty());
+//        System.out.println(myList.isEmpty());
+
+        System.out.println(myList.size()); // 0
+
+//        myList.remove(90);
+
         myList.add(10);
-        System.out.println(myList.isEmpty());
+        myList.add(20);
+        myList.add(24);
+
+//        System.out.println(myList.size()); // 3
+
+        myList.add(11);
+        myList.add(99);
+
+        myList.remove(11);
+
+//        System.out.println(myList.size()); // 5
+        myList.display();
+        System.out.println();
+
+//        System.out.println(myList.get(4));
+//        System.out.println(myList.isEmpty());
     }
 }
